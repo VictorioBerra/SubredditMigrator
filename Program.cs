4a6dbca16125c69a61f4115f98cdb88802c6e1b9
Victorio Berra <toryberra@gmail.com>
2021-02-03 20:32:51 -0600

    Update to net5
    improve readme, update nugets, update to oauth for reddit, added --deleteallpostsandcomments and --migratesaved functionality.

--- Program.cs
+++ Program.cs
@@ -2,92 +2,192 @@
 using McMaster.Extensions.CommandLineUtils;
 using System.Linq;
 using RedditSharp;
+using RedditSharp.Things;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace reddit_importer
 {
     class Program
     {
-        [Option("--targetusername")]
+        [Required]
+        [Option("--targetusername", "The account that will be migrated. This account has your desired subs and saved things.", CommandOptionType.SingleValue)]
         public string TargetUsername { get; set; }
 
+        [Required]
         [Option("--targetpassword")]
         public string TargetPassword { get; set; }
 
-        [Option("--destusername")]
+        [Required]
+        [Option("--destusername", "The account that will end up with the target accounts subs and saved things.", CommandOptionType.SingleValue)]
         public string DestUsername { get; set; }
 
+        [Required]
         [Option("--destpassword")]
         public string DestPassword { get; set; }
+
+        [Required]
+        [Option("--clientid")]
+        public string clientid { get; set; }
+
+        [Required]
+        [Option("--clientsecret")]
+        public string clientsecret { get; set; }
+
+        [Option("--redirecturi")]
+        public string redirecturi { get; set; } = "http://localhost/";
 
         [Option]
         public bool Verbose { get; set; }
 
+        [Option("--migratesubs", "Migrate subreddit subscriptions to new account.", CommandOptionType.NoValue)]
+        public bool MigrateSubs { get; set; }
+
+        [Option("--deleteallpostsandcomments", "Delete all posts and comments on the target account.", CommandOptionType.NoValue)]
+        public bool DeleteAllPostsAndComments { get; set; }
+
+        [Option("--migratesaved", "Migrate saved posts and comments to new account.", CommandOptionType.NoValue)]
+        public bool MigrateSaved { get; set; }
+
         public static int Main(string[] args)
             => CommandLineApplication.Execute<Program>(args);
 
-        private void OnExecute()
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Called by CommandLineApplication")]
+        private async System.Threading.Tasks.Task OnExecuteAsync()
         {
-
-            if (string.IsNullOrEmpty(TargetUsername) ||
-                string.IsNullOrEmpty(TargetPassword) ||
-                string.IsNullOrEmpty(DestUsername) ||
-                string.IsNullOrEmpty(DestPassword))
+            if(!this.MigrateSubs && !this.DeleteAllPostsAndComments && !this.MigrateSaved)
             {
-                throw new Exception("missing required info");
+                Console.WriteLine("Please specify a flag. See --help.");
+                return;
             }
 
-            Console.WriteLine($"Attempting login for {TargetUsername}");  
-
-            var importReddit = new Reddit();
-            importReddit.LogIn(TargetUsername, TargetPassword);
-            var importAccount = importReddit.User;
-
-            Console.WriteLine($"Attempting login for {DestUsername}");  
-
-            var exportReddit = new Reddit();
-            exportReddit.LogIn(DestUsername, DestPassword);
-
-            Console.WriteLine($"Starting subreddit migration.");  
-
-            Log(Verbose, $"Enumerating subreddit count for {TargetUsername}.");
-
-            int totalSubscriptions = importAccount.SubscribedSubreddits.Count();
-            int currentSubscription = 0;
-
-            Log(Verbose, $"Getting names for {totalSubscriptions} subreddits");
-
-            foreach (string subreddit in importAccount.SubscribedSubreddits.Select(x => x.Name))
+            if(this.MigrateSubs)
             {
-                Console.WriteLine($"Subscribing {exportReddit.User} to {subreddit} ({currentSubscription + 1} of {totalSubscriptions})");  
-
-                currentSubscription++;
-                
-                RedditSharp.Things.Subreddit subredditToResubscribe = null;
-
-                try
-                {
-                    subredditToResubscribe = exportReddit.GetSubreddit(subreddit);
-
-                    if(subredditToResubscribe == null)
-                    {
-                        Console.WriteLine($"Failed to fetch {subreddit}. Skipping subscription.");  
-                    }
-                    else
-                    {
-                        subredditToResubscribe.Subscribe();
-                    }
-
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine($"Failed to fetch {subreddit}. Skipping subscription.");
-
-                    Log(Verbose, ex.Message);
-                }
-                
-                Log(Verbose, $"Successfully subscribed {exportReddit.User} to {subreddit}");
-                
+                Console.WriteLine("I will migrate your subreddits.");
             }
+
+            if (this.MigrateSaved)
+            {
+                Console.WriteLine("I will migrate your saved items.");
+            }
+
+            if (this.DeleteAllPostsAndComments)
+            {
+                Console.WriteLine("I will delete all your comments and posts.");
+            }
+
+            var result = Prompt.GetYesNo("Do you want to proceed?", false);
+            if(!result)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Attempting login for {TargetUsername}");
+            var targetRedditAgent = new BotWebAgent(TargetUsername, TargetPassword, clientid, clientsecret, redirecturi);
+            var targetReddit = new Reddit(targetRedditAgent, initUser: true);
+            Console.WriteLine($"Success.");
+
+            Console.WriteLine($"Attempting login for {DestUsername}");
+            var destinationRedditAgent = new BotWebAgent(DestUsername, DestPassword, clientid, clientsecret, redirecturi);
+            var destinationReddit = new Reddit(destinationRedditAgent, initUser: true);
+            Console.WriteLine($"Success.");
+
+            if (MigrateSubs)
+            {
+                Console.WriteLine($"Starting subreddit migration.");
+
+                await targetReddit.User.GetSubscribedSubreddits().ForEachAsync(async (subreddit, index) => {
+
+                    try
+                    {
+                        Console.WriteLine($"({index}) - Fetching {subreddit.Name}.");
+                        var subredditToResubscribe = await destinationReddit.GetSubredditAsync(subreddit.Name, validateName: false);
+                        await subredditToResubscribe.SubscribeAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to fetch {subreddit}. Skipping subscription.");
+                        Log(Verbose, ex.Message);
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Skipping subreddit migration. Use --migratesubs");
+            }
+
+            if(DeleteAllPostsAndComments)
+            {
+                await targetReddit.User.GetComments(limit: 100).ForEachAsync(async (comment, index) =>
+                {
+                    try
+                    {
+                        Console.WriteLine($"({index}) - Deleting comment {comment.Id}.");
+                        await comment.DelAsync();
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to fetch {comment.Id}. Skipping comment.");
+                        Log(Verbose, ex.Message);
+                    }
+                });
+                await targetReddit.User.GetPosts(limit: 100).ForEachAsync(async (post, index) =>
+                {
+                    try
+                    {
+                        Console.WriteLine($"({index}) - Deleting post {post.Id}.");
+                        await post.DelAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to fetch {post.Id}. Skipping post.");
+                        Log(Verbose, ex.Message);
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Skipping delete all posts and comments. Use --deleteallpostsandcomments");
+            }
+
+            if (MigrateSaved)
+            {
+                Console.WriteLine("Migrating saved.");
+
+                await targetReddit.User.GetSaved().ForEachAsync(async (thing, index) =>
+                {
+                    try
+                    {
+                        Console.WriteLine($"({index}) - Getting saved thing {thing.Id}.");
+                        var savedThingDest = await destinationReddit.GetThingByFullnameAsync(thing.FullName);
+                        if (savedThingDest is not null)
+                        {
+                            switch (savedThingDest)
+                            {
+                                case Comment:
+                                    await ((Comment)savedThingDest).SaveAsync();
+                                    break;
+                                case Post:
+                                    await ((Post)savedThingDest).SaveAsync();
+                                    break;
+                                default:
+                                    throw new NotImplementedException("Unsupported saved thing.");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to fetch {thing.Id}. Skipping comment.");
+                        Log(Verbose, ex.Message);
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Skipping migrating saved posts. Use --migratesaved");
+            }
+
+            Console.WriteLine("Done!");
+
         }
 
         private static void Log(bool verbose, string message){
--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# SubredditMigrator
-Migrate a large number of subreddits between accounts.
+﻿# SubredditMigrator
+
+Migrate subreddits and saved posts between 2 accounts.
+
+# Install
+
+Grab the exe from releases.
+
+# Building From Sauce
+
+Should support cross-plat since its net5.0. Clone, change .csproj runtime/output, publish, enjoy.
+
+## Set Up
+
+Go here 👉 https://www.reddit.com/prefs/apps clickthe buttom at the bottom, should say "create an app" or "create another app"
+
+**Name** can be whatever, be sure to select **script**, for **redirect uri** use `http://localhost/`
+
+Grab your new Client ID, and Client Secret.
+
+## Usage
 
 Compile and run with these arguments:
 
@@ -8,4 +27,16 @@ Compile and run with these arguments:
 --targetpassword <targetpassword>
 --destusername <destusername>
 --destpassword <destpassword>
+--clientid <clientid>
+--clientsecret <clientsecret>
+--migratesubs
+--migratesaved
+--deleteallpostsandcomments
 ```
+
+Sample command:
+
+`.\reddit-importer.exe --targetusername "Account1" --targetpassword "Account1Password" --destusername "Account2" --destpassword "Account2Password" --clientid "MYCLIENTID" --clientsecret "SfXOo9YUEdwVu6fIm6BEOt6okVFx6A" --migratesubs --migratesaved`
+
+
+Saved items only include posts and comments for now.
